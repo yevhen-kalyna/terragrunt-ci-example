f8671e88402203738f1e8c77d962a566a097ba4c
Yevhen Kalyna <yevhen.kalyna@gmail.com>
2024-02-09 11:50:48 +0200

    Refactor parse_paths.py and main.yml to exclude staging and production folders

--- .github/parse_paths.py
+++ .github/parse_paths.py
@@ -28,26 +28,34 @@ def find_affected_modules(repository_dir: str, changed_modules: list):
                 affected_modules.append(os.path.relpath(os.path.join(root, module), repository_dir))
     return affected_modules
 
-def filter_paths(paths):
+def filter_paths(paths: List, exclude_filters=None):
     # Sort paths to ensure parent paths are considered first
     paths_sorted = sorted(paths, key=lambda x: x.count('/'))
 
     filtered_paths = []
+    excluded_paths = []
     for path in paths_sorted:
         # Check if current path is a subpath of any path in filtered_paths
         if not any(path.startswith(parent_path + '/') for parent_path in filtered_paths):
+            if exclude_filters:
+                for exclude_filter in exclude_filters:
+                    if exclude_filter in path:
+                        excluded_paths.append(path)
+                        break
+                continue
             filtered_paths.append(path)
 
-    return filtered_paths
+    return filtered_paths, excluded_paths
     
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse paths")
     parser.add_argument("paths", nargs="*", help="Paths to parse")
+    parser.add_argument("--exclude", nargs="*", help="Exclude filters")
     args = parser.parse_args()
 
     paths: List = args.paths
-
+    exclude_filters = args.exclude
     if not paths:
         print("No paths to parse")
         exit(1)
@@ -81,17 +89,20 @@ if __name__ == "__main__":
             exit(1)
     affected_tg_modules = find_affected_modules(os.getcwd(), changed_tf_modules + changed_envcommon_modules)
     
-    changed_modules = filter_paths(affected_tg_modules + changed_terragrunt_folders + changed_hcl_folders)
+    changed_modules, excluded_folders = filter_paths(
+        affected_tg_modules + changed_terragrunt_folders + changed_hcl_folders,
+        exclude_filters
+    )
     # print("Changed terragrunt folders: ", changed_terragrunt_folders)
     # print("Changed hcl folders: ", changed_hcl_folders)
     # print("Changed envcommon modules: ", changed_envcommon_modules)
     # print("Changed tf modules: ", changed_tf_modules)
     print("Changed modules: ", changed_modules)
     # Generate fromJSON output, to use matrix strategy in the workflow
-    changed_modules_json = json.dumps(changed_modules)
     try:
       with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-          print(f'CHANGED_FOLDERS={changed_modules_json}', file=fh)
+          print(f'CHANGED_FOLDERS={json.dumps(changed_modules)}', file=fh)
+          print(f'EXCLUDED_FOLDERS={json.dumps(excluded_folders)}', file=fh)
     except KeyError:
       print("GITHUB_OUTPUT environment variable not set. Exiting.")
       exit(1)
